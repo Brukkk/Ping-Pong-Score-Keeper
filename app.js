523ff0404129f50a6d2c18c96ff80a02914d197f
Brukkk <bruktorra@gmail.com>
2021-12-14 15:44:58 -0300

    finish logic on buttons

--- app.js
+++ app.js
@@ -6,15 +6,40 @@ const reset = document.querySelector("#reset");
 
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 6;
+
+function someoneWins(){
+    if (p1Score === winningScore || p2Score === winningScore){
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
+        p1Btn.classList.toggle("disabled");
+        p2Btn.classList.toggle("disabled");
+    }
+}
 
 p1Btn.addEventListener("click",()=>{
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+    if (p1Score !== winningScore){
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    } 
+    if (p1Score === winningScore) {
+        someoneWins();
+        p1Display.classList.add("winner");
+        p2Display.classList.add("looser");
+    }
+    
 });
 
 p2Btn.addEventListener("click", ()=>{
-    p2Score += 1;
-    p2Display.textContent = p2Score;
+    if (p2Score !== winningScore){
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+    } 
+    if (p2Score === winningScore)  {
+        someoneWins();
+        p2Display.classList.add("winner");
+        p1Display.classList.add("looser");
+    }
 });
 
 reset.addEventListener("click", ()=>{
@@ -22,4 +47,11 @@ reset.addEventListener("click", ()=>{
     p2Score = 0;
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
-})
+    p1Btn.disabled = false;
+    p1Btn.classList.toggle("disabled");
+    p2Btn.disabled = false;
+    p2Btn.classList.toggle("disabled");
+    p1Display.classList.remove("winner","looser");
+    p2Display.classList.remove("winner","looser");
+});
+
