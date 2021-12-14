094cbaf2c78997dce3aed4dc717b9319d2b54878
Brukkk <bruktorra@gmail.com>
2021-12-14 16:30:08 -0300

    reset button bug due to toggle

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const p2Display = document.querySelector("#p2");
 const p1Btn = document.querySelector("#p1Btn");
 const p2Btn = document.querySelector("#p2Btn");
 const reset = document.querySelector("#reset");
-
+const selectScore = document.querySelector("#score");
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 6;
@@ -12,8 +12,8 @@ function someoneWins(){
     if (p1Score === winningScore || p2Score === winningScore){
         p1Btn.disabled = true;
         p2Btn.disabled = true;
-        p1Btn.classList.toggle("disabled");
-        p2Btn.classList.toggle("disabled");
+        p1Btn.classList.add("disabled");
+        p2Btn.classList.add("disabled");
     }
 }
 
@@ -48,10 +48,13 @@ reset.addEventListener("click", ()=>{
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
     p1Btn.disabled = false;
-    p1Btn.classList.toggle("disabled");
+    p1Btn.classList.remove("disabled");
     p2Btn.disabled = false;
-    p2Btn.classList.toggle("disabled");
+    p2Btn.classList.remove("disabled");
     p1Display.classList.remove("winner","looser");
     p2Display.classList.remove("winner","looser");
 });
 
+selectScore.addEventListener("click",()=>{
+    winningScore = parseInt(selectScore.value);
+})
